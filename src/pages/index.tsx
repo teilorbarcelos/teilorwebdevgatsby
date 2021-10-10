8784025cc9b2c5037c8528fb7fb7c6d0588570b0
Teilor Souza Barcelos <tsb-@live.com>
2021-10-10 14:58:51 -0300

    initializing styles variable, @fontsource/barlow-condensed added

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./global.scss"
+import { styles } from "./styles"
+
+const IndexPage = () => {
+  return (
+    <main style={styles.main}>
+      <h1 style={styles.title}>Hello gatsby!</h1>
+    </main>
+  )
+}
+
+export default IndexPage
---
+++ src/pages/styles.ts
@@ -0,0 +1,12 @@
+export const styles = {
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'gray',
+    height: '100%'
+  },
+  title: {
+
+  }
+}
